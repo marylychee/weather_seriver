165b6db270dff02cc18c940720d23096a250e459
Mary Lai <marylychee@gmail.com>
2017-07-26 20:59:50 +1000

    allow cors

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const app = express();
 const port = process.env.PORT || 8000;
 const {env: {DARKSKY_API_KEY}} = process;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/weather', function(req, res) {
 	const {lng, lat} = req.query;
